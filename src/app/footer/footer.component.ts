b71c5f87eef2a0ee80a39ab2ba2f18cc5940422c
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-11 14:27:20 +0800

    自訂輸入屬性名稱 ( 讓元件內的屬性名稱與在範本使用的屬性名稱不一樣 )

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  @Input()
+  @Input('footerTodos')
   todos: any[];
 
   constructor() { }
