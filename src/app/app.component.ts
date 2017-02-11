4db97e434b7b5cc27442b1eef57651c3d7fa20e3
Bo-Xuan Fan <coffee0127@gmail.com>
2017-02-11 17:32:51 +0800

    任務 14：注入 Http 服務元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -15,6 +16,9 @@ export class AppComponent {
 
   todo: string;
 
+  constructor(private http: Http) {
+  }
+
   addTodo(item: HTMLInputElement) {
     if (item.value) {
       this.todos = [...this.todos, { value: item.value, done: false }];
@@ -24,5 +28,20 @@ export class AppComponent {
 
   clearCompleted() {
     this.todos = this.todos.filter(v => !v.done);
+  }
+
+  toggleAll(status: boolean) {
+    this.todos = this.todos.map(item => {
+      item.done = status;
+      return item;
+    });
+  }
+
+  remove(item) {
+    // Angular2 使用 splice 會有問題 ==> 所指向的陣列位置沒有改變，因此不會執行變更偵測
+    let index = this.todos.indexOf(item);
+    this.todos.splice(index, 1);
+    // 因此要給予一個全新的陣列
+    this.todos = [...this.todos];
   }
 }
